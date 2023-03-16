bc1680f338ed09e35dba5c8bc7ce59e54c4ffa23
Leeann Saengsavang <leeann.saengsavang@gmail.com>
2023-03-15 21:44:09 -0700

    Need CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,10 @@
-// TODO: Declare any global variables we need
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
     let total = 0;
     let numberOfHeads = 0;
     let numberOfTails = 0;
     let percentageOfHeads = 0;
     let percentageOfTails = 0;
 
-
-    // TODO: Add event listener and handler for flip and clear buttons
     const flipButton = document.querySelector('#flip');
     const clearButton = document.querySelector('#clear');
     const statusMessage = document.querySelector('#statusMessage');
@@ -55,24 +48,5 @@ document.addEventListener('DOMContentLoaded', function () {
         statusMessage.textContent = "Let's Get Rolling!";
         pennyImage.src = 'assets/images/penny-heads.jpg';
     })
-
-    
-
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
